98638d4c43616af59bffcf771ce6e683ef8011b6
Oscar Toledo <oscartoledo1799@gmail.com>
2024-08-17 11:53:55 -0500

    Inicialize the MedicalApp

---
+++ src/Doctor.java
@@ -0,0 +1,12 @@
+public class Doctor {
+    // atributes
+    int id;
+    String name;
+    String speciality;
+
+    // Methods
+    public void showName() {
+        System.out.println(name);
+    }
+
+}
